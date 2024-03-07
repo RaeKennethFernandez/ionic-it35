cf2d8302aed4d4d5c3db28cfa82db2eab6c05524
RaeKennethFernandez <rk.fernandez@cmu.edu.ph>
2024-03-07 10:18:16 +0800

    added base on vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://raekennethfernandez.github.io/ionic-it35/',
   plugins: [
     react(),
     legacy()
